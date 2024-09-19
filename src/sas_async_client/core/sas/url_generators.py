77e0591e34784621979f0eb7ba8f09efcd8272d6
a129924 <a129924@gmail.com>
2024-09-19 17:11:58 +0800

    feat: 新增 CAS API 相關的 URL 生成函數
    - 新增 generate_cas_base_url 函數以生成 CAS REST 基本 URL。
    - 新增 generate_cas_sessions_url 函數以生成 CAS REST 會話列表 URL。
    - 新增 generate_cas_session_url 函數以生成 CAS REST 會話 URL。
    - 新增 generate_cas_session_id_url 函數以生成特定 CAS 會話 ID 的 URL。
    - 新增 generate_cas_action_url 函數以生成 CAS REST 行動 URL 並執行行動。

---
+++ src/sas_async_client/core/sas/url_generators.py
@@ -0,0 +1,198 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1)
+def generate_cas_base_url(base_url: str) -> str:
+    """
+    Generate the CAS REST base URL.
+
+    GET:
+        - /cas
+
+        Get information about the CAS servers.
+
+    Args:
+        base_url (str): The base URL.
+
+    Returns:
+        str: The CAS REST base URL.
+    """
+    return f"{base_url}/cas-shared-default-http/cas"
+
+
+@lru_cache(maxsize=1)
+def generate_cas_sessions_url(base_url: str) -> str:
+    """
+    Generate the CAS REST sessions URL.
+
+    GET:
+        - /cas/sessionList
+
+        Get a list of active sessions. Does not return information that requires connecting to the sessions for retrieval.
+
+    Args:
+        base_url (str): The base URL.
+
+    Returns:
+        str: The CAS REST sessions URL.
+
+    Schema:
+    ```json
+        [
+            {
+                "links": [
+                    {
+                        "rel": "self",
+                        "method": "GET" | "POST" | "PUT" | "DELETE",
+                        "href": "{cas_rest_base_url}/cas/sessionList",
+                        "uri": "{cas_rest_base_url}/cas/sessionList",
+                        "type": "application/json"
+                    },
+                    ...
+                ],
+                "uuid": "{uuid}",
+                "id": "{id}",
+                "name": "{session_name}",
+                "user": "{username}",
+                "owner": "{username}",
+                "state": "connected" | "disconnected"
+            },
+            ...
+        ]
+    ```
+    """
+    return f"{generate_cas_base_url(base_url)}/sessionList"
+
+
+@lru_cache(maxsize=1)
+def generate_cas_session_url(base_url: str) -> str:
+    """
+    Generate the CAS REST session URL And Post Session ID.
+
+    GET:
+        - /cas/sessions
+
+        Get information about the CAS session.
+
+    POST:
+        - /cas/sessions
+
+        Create a new CAS session.
+
+    DELETE:
+        - /cas/sessions/{cas_session_id}
+
+        Delete a CAS session.
+
+    Args:
+        base_url (str): The base URL.
+
+    Returns:
+        str: The CAS REST session URL And Get Session ID.
+
+    Schema:
+
+        GET:
+    ```json
+        {
+            "links": [
+                {
+                    "rel": "self",
+                    "method": "GET" | "POST" | "PUT" | "DELETE",
+                    "href": "{cas_rest_base_url}/cas/sessions",
+                    "uri": "{cas_rest_base_url}/cas/sessions",
+                    "type": "application/json"
+                },
+                ...
+            ],
+            "uuid": "{uuid}",
+            "id": "{id}",
+            "name": "{session_name}",
+            "user": "{username}",
+            "owner": "{username}",
+            "state": "connected" | "disconnected",
+            "provider": "OAuth",
+            "activeAction": "{last_finished_action_name}"
+            ...
+        },
+        ...
+    ]
+
+
+    ```
+    POST:
+    ```json
+        {
+            "session": "{cas_session_id}"
+        }
+    ```
+    """
+    return f"{generate_cas_sessions_url(base_url)}/sessions"
+
+
+@lru_cache(maxsize=1)
+def generate_cas_session_id_url(base_url: str, cas_session_id: str) -> str:
+    """
+    Generate the CAS REST session ID URL.
+
+    Args:
+        cas_session_id (str): The CAS session ID.
+
+    Returns:
+        str: The CAS REST session ID URL.
+    """
+    return f"{generate_cas_session_url(base_url)}/{cas_session_id}"
+
+
+def generate_cas_action_url(base_url: str, cas_session_id: str) -> str:
+    """
+    Generate the CAS REST action URL and shutdown last actions and get all actions info.
+
+    GET:
+        - /cas/sessions/{cas_session_id}/actions
+
+        Get information about the All CAS actions.
+
+    DELETE:
+        - /cas/sessions/{cas_session_id}/actions
+
+        Cancels the currently running action by sending a cancelAction action.
+
+    Args:
+        base_url (str): The base URL.
+        cas_session_id (str): The CAS session ID.
+
+    Returns:
+        str: The CAS REST action URL.
+    """
+    return f"{generate_cas_session_id_url(base_url, cas_session_id)}/actions"
+
+
+def generate_cas_action_name_url(
+    base_url: str,
+    cas_session_id: str,
+    action_name: str,
+) -> str:
+    """
+    Generate the CAS REST action URL and run action.
+
+    GET:
+        - /cas/sessions/{cas_session_id}/actions/{action_name}
+
+        Get information about the CAS action.
+
+    POST:
+        - /cas/sessions/{cas_session_id}/actions/{action_name}
+
+        Run a new CAS action.
+
+    Args:
+        base_url (str): The base URL.
+        cas_session_id (str): The CAS session ID.
+        action_name (str): The action name.
+
+    Returns:
+        str: The CAS REST action URL.
+    """
+
+    return f"{generate_cas_action_url(base_url, cas_session_id)}/{action_name}"
