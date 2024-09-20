dd76da461935b875860838c00d5dca5aa98c8ac9
a129924 <a129924@gmail.com>
2024-09-20 17:09:00 +0800

    refactor: 使用 fixture 參數化測試以獲取 SAS 存取令牌
    - 修改測試函數，將 async_web_session、config 和 token 參數化為 fixture，以提高測試的可讀性和可維護性。
    - 確保測試能夠正確使用 fixture 獲取所需的參數，簡化測試邏輯。

--- tests/sas_api/test_auth.py
+++ tests/sas_api/test_auth.py
@@ -1,28 +1,63 @@
+from pytest import fixture as sync_fixture
 from pytest import mark
+from pytest_asyncio import fixture as async_fixture
 
 
-@mark.asyncio
-async def test_get_sas_access_token():
-    from src.sas_async_client._utils.connection_utils import get_sas_api_connect_config
+@sync_fixture(scope="module")
+def client():
     from src.sas_async_client.core._httpx import create_async_web_session
+
+    return create_async_web_session()
+
+
+@sync_fixture(scope="module")
+def config():
+    from src.sas_async_client._utils.connection_utils import get_sas_api_connect_config
+
+    return get_sas_api_connect_config()  # 返回配置供測試使用
+
+
+@async_fixture(scope="module")
+async def token(client, config):
     from src.sas_async_client.core.sas.auth import get_sas_access_token
     from src.sas_async_client.types.oauth import OAuthToken
 
-    config = get_sas_api_connect_config()
+    token = await get_sas_access_token(
+        async_web_session=client,
+        base_url=config.sas_api_connect_url,
+        user_id=config.sas_api_connect_username,
+        user_password=config.sas_api_connect_password,
+    )
 
-    async with create_async_web_session() as client:
-        token = await get_sas_access_token(
-            client,
-            config.sas_api_connect_url,
-            config.sas_api_connect_username,
-            config.sas_api_connect_password,
-        )
+    assert isinstance(token, OAuthToken)
+    assert token.access_token
+    assert token.token_type == "bearer"
+    assert token.id_token
+    assert token.refresh_token
+    assert token.expires_in
+    assert token.scope
+    assert token.jti
 
-        assert isinstance(token, OAuthToken)
-        assert token.access_token
-        assert token.token_type == "bearer"
-        assert token.id_token
-        assert token.refresh_token
-        assert token.expires_in
-        assert token.scope
-        assert token.jti
+    return token
+
+
+@async_fixture(scope="module")
+async def cas_session_id(token, client, config):
+    from src.sas_async_client.core.sas.cas import get_cas_session_id
+
+    cas_session = await get_cas_session_id(
+        async_web_session=client,
+        sas_oauth_token=token.access_token,
+        base_url=config.sas_api_connect_url,
+    )
+    return cas_session  # 返回 CAS session ID
+
+
+@mark.asyncio
+async def test_get_sas_access_token(token):
+    assert token
+
+
+@mark.asyncio
+async def test_create_cas_session(cas_session_id):
+    assert cas_session_id
